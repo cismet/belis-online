02e7999f44c1a0377837bf89c42c72a162e2cfe6
helllth <th@cismet.de>
2020-09-25 09:40:09 +0200

    :sparkles: pale nightplan bug fixed

--- src/containers/MobileApp.js
+++ src/containers/MobileApp.js
@@ -35,7 +35,7 @@ const backgrounds = {
 	stadtplan: 'wupp-plan-live@90',
 	lbk: 'wupp-plan-live@100|trueOrtho2020@75|rvrSchrift@100',
 	nightplan:
-		'wupp-plan-live@{"opacity":0.3,"css-filter": "filter:grayscale(0.9)brightness(0.9)invert(1)"}',
+		'wupp-plan-live@{"opacity":0.9,"css-filter": "filter:grayscale(0.9)brightness(0.9)invert(1)"}',
 	pale_stadtplan: 'wupp-plan-live@30',
 	pale_lbk: 'wupp-plan-live@20|trueOrtho2020@30|rvrSchrift@100',
 	pale_nightplan:
