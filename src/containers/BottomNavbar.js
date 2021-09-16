5df07b60ea1204ad74f14d2bd87c9d9c1c78edf3
helllth <th@cismet.de>
2021-09-16 15:00:55 +0200

    wo #14: first steps towards responisve behaviour

--- src/containers/BottomNavbar.js
+++ src/containers/BottomNavbar.js
@@ -28,12 +28,14 @@ import {
   setFocusModeActive,
 } from "../core/store/slices/featureCollection";
 import { isPaleModeActive, setPaleModeActive } from "../core/store/slices/paleMode";
+import { useWindowSize } from "@react-hook/window-size";
 
 //---------
 
 const BottomNavbar = ({ innerRef, onlineStatus, refRoutedMap, jwt }) => {
   const dispatch = useDispatch();
   const browserlocation = useLocation();
+  const [windowWidth, windowHeight] = useWindowSize();
 
   const inFocusMode = useSelector(isInFocusMode);
   const cacheDate = useSelector(getCacheDate);
@@ -52,176 +54,213 @@ const BottomNavbar = ({ innerRef, onlineStatus, refRoutedMap, jwt }) => {
   } else {
     user = getLoginFromJWT(jwt);
   }
+  let fontSize, narrow, fontSizeIconPixel, iconWidth;
+
+  if (windowWidth <= 1200) {
+    fontSize = "0.8rem";
+    narrow = true;
+    fontSizeIconPixel = 18;
+    iconWidth = "24px";
+  } else {
+    fontSize = "1rem";
+    narrow = false;
+    fontSizeIconPixel = 24;
+    iconWidth = "24px";
+  }
 
   return (
-    <Navbar ref={innerRef} bg={background === "nightplan" ? "dark" : "light"} expand='lg'>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Nav className='mr-auto'>
-        <div>
-          {connectionMode === CONNECTIONMODE.ONLINE && (
-            <Icon
-              style={{ fontSize: 24, width: "30px", cursor: "pointer" }}
-              className='text-primary'
-              icon={faUser}
-              onClick={() => {
-                dispatch(setConnectionMode(CONNECTIONMODE.FROMCACHE));
-              }}
-            />
-          )}
-          {connectionMode === CONNECTIONMODE.FROMCACHE && (
-            <Icon
-              style={{ fontSize: 24, width: "30px", cursor: "pointer" }}
-              className='text-primary'
-              icon={faDatabase}
-              onClick={() => {
-                dispatch(setConnectionMode(CONNECTIONMODE.ONLINE));
-              }}
-            />
-          )}
-          {user && <span style={{ marginLeft: 10 }}>{user}</span>}
-        </div>
-        <div style={{ marginLeft: 10 }}>
-          <ButtonGroup className='mr-2' aria-label='ModusGroup'>
+    <div style={{ fontSize }}>
+      <Navbar ref={innerRef} bg={background === "nightplan" ? "dark" : "light"} expand='lg'>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Nav className='mr-auto'>
+          <div>
+            {connectionMode === CONNECTIONMODE.ONLINE && (
+              <Icon
+                style={{
+                  fontSize: fontSizeIconPixel,
+                  width: iconWidth,
+                  cursor: "pointer",
+                }}
+                className='text-primary'
+                icon={faUser}
+                onClick={() => {
+                  dispatch(setConnectionMode(CONNECTIONMODE.FROMCACHE));
+                }}
+              />
+            )}
+            {connectionMode === CONNECTIONMODE.FROMCACHE && (
+              <Icon
+                style={{
+                  fontSize: fontSizeIconPixel,
+                  width: iconWidth,
+                  cursor: "pointer",
+                }}
+                className='text-primary'
+                icon={faDatabase}
+                onClick={() => {
+                  dispatch(setConnectionMode(CONNECTIONMODE.ONLINE));
+                }}
+              />
+            )}
+            {user && <span style={{ marginLeft: 10 }}>{user}</span>}
+          </div>
+          <div style={{ marginLeft: 10 }}>
+            <ButtonGroup className='mr-2' aria-label='ModusGroup'>
+              <Button
+                style={{ fontSize }}
+                variant={connectionMode === CONNECTIONMODE.ONLINE ? "primary" : "outline-primary"}
+                onClick={() => {
+                  dispatch(setConnectionMode(CONNECTIONMODE.ONLINE));
+                }}
+              >
+                Live-Daten
+              </Button>
+              <Button
+                style={{ fontSize }}
+                disabled={isCacheReady === false}
+                variant={
+                  connectionMode === CONNECTIONMODE.FROMCACHE ? "primary" : "outline-primary"
+                }
+                onClick={() => {
+                  dispatch(setConnectionMode(CONNECTIONMODE.FROMCACHE));
+                }}
+              >
+                <span>
+                  lokale Daten{" "}
+                  {cacheDate !== -1 && (
+                    <span>
+                      (
+                      {isToday(cacheDate)
+                        ? new Date(cacheDate).toLocaleTimeString([], {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          })
+                        : new Date(cacheDate).toLocaleDateString()}
+                      )
+                    </span>
+                  )}
+                </span>
+              </Button>
+            </ButtonGroup>
+          </div>
+          {cachingProgress >= 1 && (
             <Button
-              variant={connectionMode === CONNECTIONMODE.ONLINE ? "primary" : "outline-primary"}
+              variant={"outline-primary"}
+              style={{ marginLeft: 20, fontSize }}
               onClick={() => {
-                dispatch(setConnectionMode(CONNECTIONMODE.ONLINE));
+                dispatch(renewAllSecondaryInfoCache(jwt));
+                dispatch(renewAllPrimaryInfoCache(jwt));
               }}
             >
-              Live-Daten
+              Daten aktualisieren
+            </Button>
+          )}
+          {cachingProgress < 1 && (
+            <ProgressBar
+              style={{
+                marginLeft: 29,
+                minHeight: 31.5,
+                minWidth: 141,
+                fontSize: 14,
+                borderWidth: 1,
+                borderStyle: "solid",
+                borderColor: "#377CF6",
+              }}
+              animated
+              now={cachingProgress * 100 + 10}
+              max={110}
+            />
+          )}
+        </Nav>
+
+        <Nav className='mr-auto'>
+          <Switch
+            id='focus-toggle'
+            preLabel='Fokus'
+            switched={inFocusMode}
+            stateChanged={(switched) => {
+              dispatch(setFocusModeActive(switched));
+              dispatch(
+                loadObjects({
+                  boundingBox: refRoutedMap.current.getBoundingBox(),
+                  inFocusMode: switched,
+                  jwt: jwt,
+                })
+              );
+            }}
+          />
+          <div style={{ width: 10 }} />
+          <Switch
+            id='pale-toggle'
+            preLabel='Blass'
+            switched={inPaleMode}
+            stateChanged={(switched) => dispatch(setPaleModeActive(switched))}
+          />
+        </Nav>
+
+        <Form inline>
+          <ButtonGroup className='mr-2' aria-label='First group'>
+            <Button
+              style={{ fontSize }}
+              variant={background === "stadtplan" ? "primary" : "outline-primary"}
+              onClick={() => {
+                dispatch(setBackground("stadtplan"));
+              }}
+            >
+              Stadtplan
             </Button>
             <Button
-              disabled={isCacheReady === false}
-              variant={connectionMode === CONNECTIONMODE.FROMCACHE ? "primary" : "outline-primary"}
+              style={{ fontSize }}
+              variant={background === "nightplan" ? "primary" : "outline-primary"}
               onClick={() => {
-                dispatch(setConnectionMode(CONNECTIONMODE.FROMCACHE));
+                dispatch(setBackground("nightplan"));
               }}
             >
-              <span>
-                lokale Daten{" "}
-                {cacheDate !== -1 && (
-                  <span>
-                    (
-                    {isToday(cacheDate)
-                      ? new Date(cacheDate).toLocaleTimeString([], {
-                          hour: "2-digit",
-                          minute: "2-digit",
-                        })
-                      : new Date(cacheDate).toLocaleDateString()}
-                    )
-                  </span>
-                )}
-              </span>
+              Stadtplan dunkel
+            </Button>
+            <Button
+              style={{ fontSize }}
+              variant={background === "lbk" ? "primary" : "outline-primary"}
+              onClick={() => {
+                dispatch(setBackground("lbk"));
+              }}
+            >
+              Luftbildkarte
             </Button>
           </ButtonGroup>
-        </div>
-        {cachingProgress >= 1 && (
-          <Button
-            variant={"outline-primary"}
-            style={{ marginLeft: 20 }}
-            onClick={() => {
-              dispatch(renewAllSecondaryInfoCache(jwt));
-              dispatch(renewAllPrimaryInfoCache(jwt));
-            }}
-          >
-            Daten aktualisieren
-          </Button>
+        </Form>
+        {uiThreadProgressbar === true && (
+          <Nav>
+            <ProgressBar style={{ width: 200 }} animated now={100} max={100} />
+          </Nav>
         )}
-        {cachingProgress < 1 && (
-          <ProgressBar
-            style={{
-              marginLeft: 29,
-              minHeight: 31.5,
-              minWidth: 141,
-              fontSize: 14,
-              borderWidth: 1,
-              borderStyle: "solid",
-              borderColor: "#377CF6",
-            }}
-            animated
-            now={cachingProgress * 100 + 10}
-            max={110}
-          />
-        )}
-      </Nav>
-
-      <Nav className='mr-auto'>
-        <Switch
-          disabled={false}
-          id='focus-toggle'
-          preLabel='Fokus'
-          switched={inFocusMode}
-          stateChanged={(switched) => {
-            dispatch(setFocusModeActive(switched));
-            dispatch(
-              loadObjects({
-                boundingBox: refRoutedMap.current.getBoundingBox(),
-                inFocusMode: switched,
-                jwt: jwt,
-              })
-            );
-          }}
-        />
-
-        <div style={{ width: 30 }} />
-        <Switch
-          id='pale-toggle'
-          preLabel='Blass'
-          switched={inPaleMode}
-          stateChanged={(switched) => dispatch(setPaleModeActive(switched))}
-        />
-      </Nav>
-
-      <Form inline>
-        <ButtonGroup className='mr-2' aria-label='First group'>
-          <Button
-            variant={background === "stadtplan" ? "primary" : "outline-primary"}
-            onClick={() => {
-              dispatch(setBackground("stadtplan"));
-            }}
-          >
-            Stadtplan
-          </Button>
-          <Button
-            variant={background === "nightplan" ? "primary" : "outline-primary"}
-            onClick={() => {
-              dispatch(setBackground("nightplan"));
-            }}
-          >
-            Stadtplan dunkel
-          </Button>
-          <Button
-            variant={background === "lbk" ? "primary" : "outline-primary"}
-            onClick={() => {
-              dispatch(setBackground("lbk"));
-            }}
-          >
-            Luftbildkarte
-          </Button>
-        </ButtonGroup>
-      </Form>
-      {uiThreadProgressbar === true && (
         <Nav>
-          <ProgressBar style={{ width: 200 }} animated now={100} max={100} />
+          <div>
+            {onlineStatus ? (
+              <Icon
+                style={{
+                  fontSize: fontSizeIconPixel,
+                  width: iconWidth,
+                  cursor: "pointer",
+                }}
+                className='text-primary'
+                icon={faWifi}
+              />
+            ) : (
+              <Icon
+                style={{
+                  fontSize: fontSizeIconPixel,
+                  width: iconWidth,
+                  cursor: "pointer",
+                  color: "#dddddd",
+                }}
+                icon={faWifi}
+              />
+            )}
+          </div>
         </Nav>
-      )}
-      <Nav>
-        <div>
-          {onlineStatus ? (
-            <Icon
-              style={{ fontSize: 24, width: "30px", cursor: "pointer" }}
-              className='text-primary'
-              icon={faWifi}
-            />
-          ) : (
-            <Icon
-              style={{ fontSize: 24, width: "30px", cursor: "pointer", color: "#dddddd" }}
-              icon={faWifi}
-            />
-          )}
-        </div>
-      </Nav>
-    </Navbar>
+      </Navbar>
+    </div>
   );
 };
 export default BottomNavbar;
