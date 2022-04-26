76eb4e6456f13ff0628c03ec209abdee2771b29f
helllth <th@cismet.de>
2022-04-26 16:35:32 +0200

    add offlineConfig

--- src/constants/belis.js
+++ src/constants/belis.js
@@ -1,5 +1,5 @@
 export const backgrounds = {
-  stadtplan: "OMT_Klokantech_basic@90",
+  stadtplan: "klokantech_basic@90",
   lbk_: "cismetText|trueOrtho2020@40",
 
   lbk: "wupp-plan-live@100|trueOrtho2020@60", //|rvrSchrift@100",
@@ -9,6 +9,73 @@ export const backgrounds = {
   pale_lbk: "wupp-plan-live@20|trueOrtho2020@30|rvrSchrift@100",
   pale_nightplan:
     'wupp-plan-live@{"opacity":0.3,"css-filter": "filter:grayscale(0.9)brightness(0.9)invert(1)"}',
+};
+
+export const offlineConfig = {
+  rules: [
+    {
+      origin: "https://omt.map-hosting.de/fonts/Metropolis Medium Italic,Noto",
+      cachePath: "fonts/Open",
+    },
+    {
+      origin: "https://omt.map-hosting.de/fonts/Klokantech Noto",
+      cachePath: "fonts/Open",
+    },
+    {
+      origin: "https://omt.map-hosting.de/fonts",
+      cachePath: "fonts",
+    },
+    {
+      origin: "https://omt.map-hosting.de/styles",
+      cachePath: "styles",
+    },
+
+    {
+      origin: "https://omt.map-hosting.de/data/v3",
+      cachePath: "tiles",
+    },
+
+    {
+      origin: "https://omt.map-hosting.de/data/gewaesser",
+      cachePath: "tiles.gewaesser",
+    },
+
+    {
+      origin: "https://omt.map-hosting.de/data/kanal",
+      cachePath: "tiles.kanal",
+    },
+
+    {
+      origin: "https://omt.map-hosting.de/data/brunnen",
+      cachePath: "tiles.brunnen",
+      // realServerFallback: true, //this can override the globalsetting
+    },
+  ],
+  dataStores: [
+    {
+      name: "Vektorkarte für Wuppertal",
+      key: "wuppBasemap",
+      url: "https://offline-data.cismet.de/offline-data/wupp.zip",
+    },
+
+    {
+      name: "Gewässer, Kanal und Brunnendaten",
+      key: "umweltalarm",
+      url: "https://offline-data.cismet.de/offline-data/umweltalarm.zip",
+    },
+  ],
+  offlineStyles: [
+    "https://omt.map-hosting.de/styles/cismet-light/style.json",
+    "https://omt.map-hosting.de/styles/osm-bright-grey/style.json",
+    "https://omt.map-hosting.de/styles/klokantech-basic/style.json",
+    "https://omt.map-hosting.de/styles/brunnen/style.json",
+    "https://omt.map-hosting.de/styles/kanal/style.json",
+    "https://omt.map-hosting.de/styles/gewaesser/style.json",
+  ],
+  realServerFallback: true, //should be true in production
+  consoleDebug: process.env.NODE_ENV !== "production",
+  optional: true,
+  initialActive: true,
 };
 
 // export const REST_SERVICE = "http://bender:8890";
@@ -26,3 +93,62 @@ export const getWebDavUrl = (jwt, doc) => {
 };
 
 export const DOMAIN = "BELIS2-TEST";
+
+export const backgroundConfigurations = {
+  lbk: {
+    layerkey: "rvrGrau@50|trueOrtho2020@40",
+    src: "/images/rain-hazard-map-bg/ortho.png",
+    title: "Luftbildkarte",
+  },
+
+  vectorCityMap2: {
+    layerkey: "cismetLight",
+    src: "/images/rain-hazard-map-bg/citymap.png",
+    title: "Stadtplan",
+  },
+
+  vectorCityMap: {
+    layerkey: "osmBrightOffline",
+    src: "/images/rain-hazard-map-bg/citymap.png",
+    title: "Stadtplan",
+  },
+
+  abkg: {
+    layerkey: "bplan_abkg@70",
+    src: "/images/rain-hazard-map-bg/citymap.png",
+    title: "Amtliche Basiskarte",
+  },
+  stadtplan: {
+    layerkey: "wupp-plan-live@90",
+    src: "/images/rain-hazard-map-bg/citymap.png",
+    title: "Stadtplan",
+  },
+  nix: {
+    layerkey: "empty",
+    src: "/images/rain-hazard-map-bg/citymap.png",
+    title: "Stadtplan",
+  },
+};
+
+export const backgroundModes = [
+  {
+    title: "Stadtplan",
+    mode: "default",
+    layerKey: "stadtplan",
+  },
+  {
+    title: "Stadtplan (bunt)",
+    mode: "default",
+    layerKey: "vectorCityMap",
+    offlineDataStoreKey: "wuppBasemap",
+  },
+  {
+    title: "Stadtplan (light)",
+    mode: "default",
+    layerKey: "vectorCityMap2",
+    offlineDataStoreKey: "wuppBasemap",
+  },
+
+  { title: "Luftbildkarte", mode: "default", layerKey: "lbk" },
+  { title: "-", mode: "default", layerKey: "nix" },
+];
