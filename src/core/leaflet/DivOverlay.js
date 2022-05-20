f3857f9ee91d4170367f06a8cfa5f9f5c0e0d224
helllth <th@cismet.de>
2022-05-20 10:59:44 +0200

    try to have a better pale mode (WIP)

---
+++ src/core/leaflet/DivOverlay.js
@@ -0,0 +1,19 @@
+import { Gridlayer } from "react-leaflet";
+
+export class DivLayer_ extends GridLayer {
+  createLeafletElement(props) {
+    return L.DivLayer();
+  }
+  componentDidMount() {
+    super.componentDidMount();
+    this.setFilter();
+  }
+  updateLeafletElement(fromProps, toProps) {
+    super.updateLeafletElement(fromProps, toProps);
+    this.setFilter();
+  }
+}
+
+const DivLayer = DivLayer_;
+
+export default DivLayer;
