83d4cd0bd8f88ff85c657cca645d1e962b6c97a8
helllth <th@cismet.de>
2022-06-01 17:49:55 +0200

    reload cache when selectedTeam changed

--- src/components/app/menu/Teams.js
+++ src/components/app/menu/Teams.js
@@ -3,15 +3,21 @@ import { useEffect, useState } from "react";
 // import dexieworker from "workerize-loader!../../../core/workers/dexie"; // eslint-disable-line import/no-webpack-loader-syntax
 import Button from "react-bootstrap/Button";
 import { useDispatch, useSelector } from "react-redux";
+import { CONNECTIONMODE, getConnectionMode } from "../../../core/store/slices/app";
 import { getJWT } from "../../../core/store/slices/auth";
-import { renewCache } from "../../../core/store/slices/cacheControl";
+import { config, getCacheSettings, renewCache } from "../../../core/store/slices/cacheControl";
 import { getWorker } from "../../../core/store/slices/dexie";
+import { loadTaskLists } from "../../../core/store/slices/featureCollection";
 import { getTeam, setTeam } from "../../../core/store/slices/team";
+import CacheItem from "../cache/CacheItem";
 
 const Teams = () => {
   // const dexieW = dexieworker();
   const dispatch = useDispatch();
   const selectedTeam = useSelector(getTeam);
+  const cacheSettings = useSelector(getCacheSettings);
+
+  const connectionMode = useSelector(getConnectionMode);
   const dexieW = useSelector(getWorker);
   const [teams, setTeams] = useState([]);
   const jwt = useSelector(getJWT);
@@ -50,7 +56,39 @@ const Teams = () => {
                 <span key={"buttonspan." + index} style={{ padding: 3 }}>
                   <Button
                     onClick={() => {
-                      dispatch(setTeam(team));
+                      if (connectionMode === CONNECTIONMODE.FROMCACHE) {
+                        (async () => {
+                          const oldTeam = selectedTeam;
+                          const oldAAs = await dexieW.getAll("arbeitsauftrag");
+                          console.log("oldAAs", oldAAs);
+                          dispatch(setTeam(team));
+                          dispatch(
+                            renewCache(
+                              "arbeitsauftrag",
+                              jwt,
+                              { team: { selectedTeam: team } },
+                              () => {
+                                //success
+                                console.log("success");
+                                dispatch(
+                                  loadTaskLists({
+                                    team: team,
+                                    jwt,
+                                  })
+                                );
+                              },
+                              () => {
+                                //error
+                                console.log("something went wrong");
+                                dexieW.putArray(oldAAs, "arbeitsauftrag");
+                                dispatch(setTeam(oldTeam));
+                              }
+                            )
+                          );
+                        })();
+                      } else {
+                        dispatch(setTeam(team));
+                      }
                     }}
                     variant='outline-primary'
                     key={team + index}
@@ -62,6 +100,24 @@ const Teams = () => {
             }
           })}
       </div>
+      {connectionMode === CONNECTIONMODE.FROMCACHE && (
+        <div>
+          <hr />
+          Wenn Sie ein anderes Team auswählen, wird direkt versucht die Arbeitsaufträge in den
+          lokalen Daten zu hinterlegen. Sollte das nicht funktionieren, weil Sie momentan keine
+          Verbindung zu unseren Servern herstellen können, kann das Team nicht gewechselt werden.
+          <hr />
+          <CacheItem
+            key={"CacheItem.Team.arbeitsauftrag"}
+            config={config["arbeitsauftrag"]}
+            info={cacheSettings["arbeitsauftrag"]}
+            renew={() => {
+              dispatch(renewCache("arbeitsauftrag", jwt));
+            }}
+          />
+          <hr />
+        </div>
+      )}
     </div>
   );
 };
