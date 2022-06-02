9b4799ff4c6f16e8f51bdef4b3f62b326f2f933d
helllth <th@cismet.de>
2022-06-02 18:18:42 +0200

    wo #16: hull impl of secondary info

---
+++ src/components/commons/secondaryinfo/components/Arbeitsauftrag.js
@@ -0,0 +1,13 @@
+import { getVCard } from "../../../../core/helper/featureHelper";
+
+const getLayout4Arbeitsauftrag = ({ feature, jwt, dispatch, setIndex, setVisible }) => {
+  const vcard = getVCard(feature);
+  const title = vcard.infobox.header;
+  let mainSectionStyle = {};
+  const subSections = [];
+
+  const mainSection = <div style={mainSectionStyle}></div>;
+  return { title, mainSection, subSections };
+};
+
+export default getLayout4Arbeitsauftrag;
