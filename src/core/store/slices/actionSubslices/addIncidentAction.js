9bba915b6da61488a8abfe39912474ce65df3497
helllth <th@cismet.de>
2022-06-15 12:27:27 +0200

    wo #4: add offline  addIncidentAction

--- src/core/store/slices/actionSubslices/addIncidentAction.js
+++ src/core/store/slices/actionSubslices/addIncidentAction.js
@@ -1,9 +1,46 @@
-const x = {
-  parameters: {
-    PRUEFDATUM: "1655199819085",
-    DATUM: "1655199819085",
-    STATUS: "1",
-    PROTOKOLL_ID: "85037",
-    MONTEUR: "Cismet",
-  },
+import { getJWT, getLoginFromJWT } from "../auth";
+import { getDB, addIntermediateResult } from "../offlineActionDb";
+import uuidv4 from "uuid/v4";
+
+const addIncidentAction = (params) => {
+  return async (dispatch, getState) => {
+    const state = getState();
+    const offlineActionDb = getDB(state);
+    const jwt = getJWT(state);
+    const login = getLoginFromJWT(jwt);
+
+    const offlineAction = {
+      id: uuidv4(),
+      action: "addIncident",
+      jwt: jwt,
+      parameter: JSON.stringify(params),
+      isCompleted: false,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      applicationId: login + "@belis",
+    };
+
+    offlineActionDb.actions.insert(offlineAction);
+
+    console.log("added object to offline db to addIncident", offlineAction, params);
+
+    // const intermediateResult = {
+    //   object_type: addImageParameter.objekt_typ,
+    //   object_id: addImageParameter.objekt_id,
+    //   data: {
+    //     imageData: addImageParameter.ImageData,
+    //     ending: addImageParameter.ending,
+    //     prefix: addImageParameter.prefix,
+    //     description: addImageParameter.description,
+    //   },
+    //   ts: addImageParameter.ts,
+    //   action: "uploadDocument",
+    //   resultType: "image",
+    // };
+
+    // //add parameterInfo to intermediateResults
+    // dispatch(addIntermediateResult(intermediateResult));
+  };
 };
+
+export default addIncidentAction;
