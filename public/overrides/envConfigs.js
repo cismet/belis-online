eb8c7268d6b7ffd974b1c637e91f8e8548b2b3a3
Thorsten Hell <thorsten.hell@cismet.de>
2023-09-15 18:11:33 +0200

    added live and playground config

---
+++ public/overrides/envConfigs.js
@@ -0,0 +1,17 @@
+//Playground
+window.BELIS_ONLINE_ENV_CONFIG = {
+  REST_SERVICE: "https://belis-cloud-api.cismet.de",
+  DOMAIN: "BELIS2",
+  OFFLINE_ACTIONS_ROUTE: "offline-actions-belis-cloud.cismet.de/v1/graphql",
+  DB_VERSION: "belis-online-cloud-db-1.0",
+  PLAYGROUND: true,
+};
+
+//Live System
+// window.BELIS_ONLINE_ENV_CONFIG = {
+//   REST_SERVICE: "https://belis-mobile-api.cismet.de",
+//   DOMAIN: "BELIS2",
+//   OFFLINE_ACTIONS_ROUTE: "belis-mobile-offline-actions.cismet.de/v1/graphql",
+//   DB_VERSION: "belis-online-db-1.0",
+//   PLAYGROUND: false,
+// };
