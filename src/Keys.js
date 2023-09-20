08619975bbbeb7afacba9daa89f82e6c4efba7a3
Thorsten Hell <thorsten.hell@cismet.de>
2023-09-20 13:54:06 +0200

    change appkey in playground

--- src/Keys.js
+++ src/Keys.js
@@ -1,2 +1,4 @@
-export const appKey = "belis-online3.0";
+import { PLAYGROUND } from "./constants/belis";
+export const appKey =
+  PLAYGROUND === false ? "belis-online3.0" : "belis-online-3.0.playground.";
 export const storagePostfix = "1";
